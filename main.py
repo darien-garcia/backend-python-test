bdabf3b415d29ebd67e1fb5dd0e4a61dc4a7ef87
Itexico-HP\Darien <dgarcia@itexico.com>
2019-12-12 08:58:18 -0600

    Update main.py
    changing the way I access sqlite3 since was not working in windows

--- main.py
+++ main.py
@@ -7,27 +7,37 @@ Usage:
 from docopt import docopt
 import subprocess
 import os
+import sqlite3
 
 from alayatodo import app
 
 
 def _run_sql(filename):
     try:
-        subprocess.check_output(
-            "sqlite3 %s < %s" % (app.config['DATABASE'], filename),
-            stderr=subprocess.STDOUT,
-            shell=True
-        )
-    except subprocess.CalledProcessError, ex:
-        print ex.output
-        os.exit(1)
+        db = get_db()
+        fullPath = os.path.join(os.path.dirname(__file__), filename)
+        if os.path.exists(fullPath):
+            with open(fullPath, 'r') as f:
+                db.executescript(f.read())
+        
+    except Exception as ex:
+        print(ex.__str__())
+        os._exit(1)
 
+def get_db():
+    db = sqlite3.connect(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)) + app.config['DATABASE'])
+    )
+    db.row_factory = sqlite3.Row
+
+    return db
+    
 
 if __name__ == '__main__':
     args = docopt(__doc__)
     if args['initdb']:
         _run_sql('resources/database.sql')
         _run_sql('resources/fixtures.sql')
-        print "AlayaTodo: Database initialized."
+        print("AlayaTodo: Database initialized.")
     else:
         app.run(use_reloader=True)
