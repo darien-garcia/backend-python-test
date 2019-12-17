4db223f4861746b5d81babdcc9882be7c6c60593
Itexico-HP\Darien <dgarcia@itexico.com>
2019-12-17 10:15:37 -0600

    TASK 5: adding pagination to the todo list

--- alayatodo/todos.py
+++ alayatodo/todos.py
@@ -1,13 +1,16 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app, flash
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app, flash, abort
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from alayatodo.auth import login_required
+from alayatodo.models import todos as todos_model 
+from alayatodo import db
 
 bp = Blueprint('todos', __name__, url_prefix='/todos')
+ITEMS_PER_PAGE = 3
 
 @bp.route('/home')
 @login_required
@@ -19,18 +22,36 @@ def home():
 @bp.route('/<int:id>/')
 @login_required
 def todo(id):
-    cur = g.db.execute("SELECT * FROM todos WHERE id ='%s'" % id)
-    todo = cur.fetchone()
+    todo = get_todo_item(id)
     return render_template('todo/todo.html', todo=todo)
 
 
 @bp.route('/todo_list')
 @login_required
 def todo_list():
-    cur = g.db.execute("SELECT * FROM todos where user_id='%s'" % session['user_id'])
-    todos = cur.fetchall()
-    
-    return render_template('todo/todos.html', todos=todos)
+    page = request.args.get('page', 1, type=int)
+    query = db.session.query(todos_model).filter(
+        todos_model.user_id == session['user_id']
+        )
+    total = 10
+    todos = query.paginate(page, ITEMS_PER_PAGE, False)
+
+    next_url = url_for('todos.todo_list', page=todos.next_num) \
+        if todos.has_next else None
+    prev_url = url_for('todos.todo_list', page=todos.prev_num) \
+        if todos.has_prev else None
+    next_num=todos.next_num \
+        if todos.has_next else 0
+    prev_num=todos.prev_num \
+        if todos.has_prev else 0
+
+    return render_template('todo/todos.html', 
+                           todos=todos.items,
+                           next=next_num,
+                           prev=prev_num,
+                           page_total = total,
+                           next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/new', methods=('GET','POST'))
@@ -41,14 +62,14 @@ def post():
         error = 'Description cannot be empty'
 
     if error == None:
-
-        g.db.execute(
-            "INSERT INTO todos (user_id, description) VALUES ('%s', '%s')"
-            % (session['user_id'], request.form.get('description', ''))
-        )
-        g.db.commit()
-        flash('Todo item added successfuly')
-
+        new_todo = todos_model(
+            description=request.form.get('description', ''),
+            user_id=session['user_id']
+            )
+        db.session.add(new_todo)
+        db.session.flush()
+        db.session.commit()
+        flash("Todo item << %s >> was successfully inserted" % new_todo.description)
     if error is not None:
         flash(error)
 
@@ -57,19 +78,22 @@ def post():
 @bp.route('<int:id>/delete/', methods=('GET', 'DELETE'))
 @login_required
 def delete(id):
-    print("Deleting item id '%s'" % id)
-    g.db.execute("DELETE FROM todos where id='%s'" % id)
-    g.db.commit()
-    flash('Item successfully deleted!')
+    todo = get_todo_item(id)
+    db.session.query(todos_model).filter(todos_model.id==id).delete()
+    db.session.commit()
+    flash("Todo item << %s >> was successfully deleted" % todo.description)
     return redirect(url_for('todos.todo_list'))
 
 @bp.route('<int:id>/json/')
 @login_required
 def json_item(id):
     todo = get_todo_item(id)
-    return render_template('todo/json.html', todo="{ id:'%s', user_id:'%s', desciption:'%s' }" %(todo['id'], todo['user_id'], todo['description']))
+    
+    if todo is not None:
+        if todo.user_id != session['user_id']:
+            abort(403)
+        return render_template('todo/json.html', todo="{ id:'%s', user_id:'%s', desciption:'%s' }" %(todo.id, todo.user_id, todo.description))
+    abort(404)
 
 def get_todo_item(id):
-    todo = cur = g.db.execute("SELECT * FROM todos WHERE id ='%s'" % id)
-    todo = cur.fetchone()
-    return todo
+    return db.session.query(todos_model).filter(todos_model.id==id).first()
